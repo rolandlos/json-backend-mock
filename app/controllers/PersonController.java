00c83d3072a7867f12e05b086031e93209bdd107
Roland Loser <roland.loser@glue.ch>
2018-07-09 15:40:12 +0200

    Use JWT Token

--- app/controllers/PersonController.java
+++ app/controllers/PersonController.java
@@ -1,10 +1,15 @@
 package controllers;
 
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.fasterxml.jackson.databind.JsonNode;
 import play.Logger;
 import play.mvc.Controller;
 import play.mvc.Result;
+import utils.JWTUtil;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -21,14 +26,37 @@ public class PersonController extends Controller {
      * <code>GET</code> request with a path of <code>/</code>.
      */
     public Result getPerson() {
-        Logger.info("-------------------- new Person Request");
-        final Map<String, List<String>> headers = request().getHeaders().toMap();
-        headers.keySet().forEach(key -> {
-            Logger.info(key+" -> "+headers.get(key));
-        });
+        DecodedJWT jwt = JWTUtil.getToken(request());
+        String email = jwt.getClaims().get("email").asString();
+        if (jwt != null) {
+            try {
+                return ok(new File(email + ".json")).withHeader("Access-Control-Allow-Origin", "*").as("application/json");
+            } catch (Exception e) {
+                return notFound("User with email "+email+" not found");
+            }
+        } else {
+            return unauthorized();
+        }
 
-        return ok(new File("./person.json")).withHeader("Access-Control-Allow-Origin","*").as("application/json");
+    }
 
+    public Result insertPerson() {
+        DecodedJWT jwt = JWTUtil.getToken(request());
+        String email = jwt.getClaims().get("email").asString();
+        if (jwt != null) {
+            try {
+                JsonNode node = request().body().asJson();
+                FileWriter fw = new FileWriter(new File(email + ".json"));
+                fw.write(node.toString());
+                fw.flush();
+                fw.close();
+                return ok().withHeader("Access-Control-Allow-Origin", "*");
+            } catch (Exception e) {
+                return notFound("User with email "+email+" not found");
+            }
+        } else {
+            return unauthorized();
+        }
     }
 
     public Result options() {
