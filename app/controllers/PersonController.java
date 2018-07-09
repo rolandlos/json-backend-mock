e70ef737ef9f0004e106885c0e96b44be7455fd0
Roland Loser <roland.loser@glue.ch>
2018-07-09 16:13:45 +0200

    Use JWT Token

--- app/controllers/PersonController.java
+++ app/controllers/PersonController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import play.Logger;
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -9,6 +10,7 @@ import utils.JWTUtil;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -30,6 +32,10 @@ public class PersonController extends Controller {
         if (jwt != null) {
             String email = jwt.getClaims().get("email").asString();
             try {
+                File jsonFile = new File(email+".json");
+                if (!jsonFile.exists()) {
+                    producePerson(jwt);
+                }
                 return ok(new File(email + ".json")).withHeader("Access-Control-Allow-Origin", "*").as("application/json");
             } catch (Exception e) {
                 return notFound("User with email "+email+" not found").withHeader("Access-Control-Allow-Origin", "*");
@@ -38,6 +44,27 @@ public class PersonController extends Controller {
             return unauthorized().withHeader("Access-Control-Allow-Origin", "*");
         }
 
+    }
+
+    private void producePerson(DecodedJWT jwt) {
+        String email = jwt.getClaims().get("email").asString();
+        String family_name = jwt.getClaims().get("family_name").asString();
+        String given_name = jwt.getClaims().get("given_name").asString();
+        String name = jwt.getClaims().get("name").asString();
+        String json = "{ \n"+
+                "\"firstName\": \""+given_name+"\",\n"+
+                "\"officialName\": \""+family_name+"\",\n"+
+                "\"email\": \""+email+"\"\n"+
+               "}";
+        try {
+            Logger.info("produce Person "+email+".json");
+            FileWriter fw = new FileWriter(new File(email + ".json"));
+            fw.write(json);
+            fw.flush();
+            fw.close();
+        } catch (IOException e) {
+            Logger.error("Error when producing Person for "+email,e);
+        }
     }
 
     public Result insertPerson() {
