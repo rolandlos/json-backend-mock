9a657a44ccbebf569c91d925e4cfc41817550425
Roland Loser <roland.loser@glue.ch>
2018-07-11 15:53:45 +0200

    Handle uploaded File

--- app/controllers/UploadController.java
+++ app/controllers/UploadController.java
@@ -1,13 +1,26 @@
 package controllers;
 
 import play.mvc.Controller;
+import play.mvc.Http;
 import play.mvc.Result;
+
+import java.io.File;
 
 public class UploadController extends Controller {
 
 
     public Result upload() {
-        return ok();
+        Http.MultipartFormData<File> body = request().body().asMultipartFormData();
+        Http.MultipartFormData.FilePart<File> picture = body.getFile("file");
+        if (picture != null) {
+            String fileName = picture.getFilename();
+            String contentType = picture.getContentType();
+            File file = picture.getFile();
+            return ok("File uploaded");
+        } else {
+            flash("error", "Missing file");
+            return badRequest();
+        }
     }
 
 }
