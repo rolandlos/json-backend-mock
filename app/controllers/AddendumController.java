01f01301b982218380017f42909c88c6924e7789
Roland Loser <roland.loser@glue.ch>
2018-10-17 12:25:22 +0200

    Add get and list to Addendum Controller

--- app/controllers/AddendumController.java
+++ app/controllers/AddendumController.java
@@ -9,7 +9,11 @@ import utils.JWTUtil;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import static controllers.CaseController.APPLICATION_JSON;
 
@@ -49,6 +53,49 @@ public class AddendumController extends Controller {
             Logger.error("User not authorized");
             return unauthorized();
         }
+    }
+
+    public Result get(String caseId, String addendumId) {
+        DecodedJWT jwt = JWTUtil.getToken(request());
+        if (jwt != null) {
+            String pid = PersonController.produceIdFormAuth(jwt);
+            File caseFile = new File(pid+"/"+caseId+".addendum"+"/"+addendumId);
+            if (caseFile.exists()) {
+                try {
+                    return ok(new String(Files.readAllBytes(caseFile.toPath()))).as(APPLICATION_JSON);
+                } catch (IOException e) {
+                    Logger.error("Error when reading Addendum file {}",caseFile,e);
+                    return notFound();
+                }
+            } else {
+                return notFound();
+            }
+        }
+        return unauthorized();
+    }
+
+
+    public Result list(String caseId) {
+        DecodedJWT jwt = JWTUtil.getToken(request());
+        if (jwt != null) {
+            String pid = PersonController.produceIdFormAuth(jwt);
+            File dir = new File(pid+"/"+caseId+".addendum");
+            if (dir.exists() && dir.isDirectory()) {
+                String collect = Arrays.stream(dir.listFiles()).map(f -> {
+                    try {
+                        return new String(Files.readAllBytes(f.toPath()));
+                    } catch (IOException e) {
+                        Logger.error("Error when reading file {}",f,e);
+                    }
+                    return "{}";
+                }).collect(Collectors.joining(","));
+                Logger.info("Return: [{}]",collect);
+                return ok("[" + collect + "]").as(APPLICATION_JSON);
+            } else {
+                return ok("[]").as(APPLICATION_JSON);
+            }
+        }
+        return unauthorized();
     }
 
 
